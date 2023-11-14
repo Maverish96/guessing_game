f37ad40584e6a38817a43826c8a4a9b776364306
Rishabh Raj <realrishabh9@gmail.com>
2023-11-15 04:31:28 +0530

    fff

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
+    
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
@@ -17,4 +17,5 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
 }
