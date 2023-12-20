8299d1c036baf7913f61a1a7cc3975c52d4420e0
Rishabh Raj <realrishabh9@gmail.com>
2023-12-20 23:31:14 +0530

    12 dec

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main(){
+use std::io::stdin;
 
-    
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess:String = "".to_string();
+
+    stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
